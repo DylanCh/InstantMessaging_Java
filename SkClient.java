f04baab71068e09ef3ac5437d5e097f3529fae93
hjchen2005 <hjchen2005@gmail.com>
2015-06-11 13:22:52 +0800

    Create SkClient.java
    In progress

---
+++ SkClient.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class SkClient extends JFrame implements ActionListener{
+
+	
+	public static void SkClient{
+		new SkClient;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
