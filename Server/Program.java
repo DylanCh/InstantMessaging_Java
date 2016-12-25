222bb3f6fc7e816933a5181788672965ae6b80e6
Dylan Chen <hchen004@citymail.cuny.edu>
2016-12-24 19:26:09 -0500

    Create Program.java

---
+++ Server/Program.java
@@ -0,0 +1,11 @@
+package Server;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SkServer server = new SkServer();
+		server.run();
+	}
+
+}
